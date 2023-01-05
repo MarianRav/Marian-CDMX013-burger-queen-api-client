aa9216371be0ba55e444fdc3d6ddd9602ae70f10
Marian Villavicencio <marian.mlva.16@gmail.com>
2023-01-04 20:39:21 -0600

    feat: responsive login

--- burguerqueen/src/components/auth/waiter/TakeOrder.jsx
+++ burguerqueen/src/components/auth/waiter/TakeOrder.jsx
@@ -18,7 +18,9 @@ function Menu({ user, changeUser }) {
   const [clientName, setClientName] = useState("");
   const [modal, setModal] = useState(false);
   const [message, setMessage] = useState(false);
-  const [modalMessage, setModalMessage] = useState('The order was submitted succesfully!');
+  const [modalMessage, setModalMessage] = useState(
+    "The order was submitted succesfully!"
+  );
   const [emptyOrderMessage, setEmptyOrderMessage] = useState(false);
 
   useEffect(() => {
@@ -115,11 +117,11 @@ function Menu({ user, changeUser }) {
 
     const promise = createOrder(orderInfo);
     promise.then((response) => {
-      if(response.status === 404){
-        setModalMessage('Error: the order was not submitted');
+      if (response.status === 404) {
+        setModalMessage("Error: the order was not submitted");
         showConfirmation();
-      } else{
-        setModalMessage('The order was submitted succesfully!');
+      } else {
+        setModalMessage("The order was submitted succesfully!");
         setDefaultValues();
         showConfirmation();
       }
@@ -139,147 +141,159 @@ function Menu({ user, changeUser }) {
     }, 5000);
   };
 
-  const showEmptyOrderMessage = (e) =>{
+  const showEmptyOrderMessage = (e) => {
     e.preventDefault();
-    setEmptyOrderMessage(true)
-    console.log('nooo')
+    setEmptyOrderMessage(true);
+    console.log("nooo");
     setTimeout(() => {
       setEmptyOrderMessage(false);
     }, 2000);
-  }
+  };
 
   return (
     <div className="menuContainer">
       <NavBar changeUser={changeUser} />
       <main className="menu">
-        <section className="options-menu">
-          <Button
-            filter={changeMenu}
-            value="breakfast"
-            name="Breakfast"
-            secondclass="left"
+        <div className="options-and-products-container">
+          <section className="options-menu">
+            <Button
+              filter={changeMenu}
+              value="breakfast"
+              name="Breakfast"
+              secondclass="left"
+            />
+            <Button
+              filter={changeMenu}
+              value="dinner"
+              name="Lunch & Dinner"
+              secondclass="center"
+            />
+            <div className="lineOne" />
+          </section>
+          <div className="product-card-container">
+            <>
+              {optionFood === "breakfast"
+                ? breakfast.map((item) => {
+                    return (
+                      <ProductCard
+                        key={item.id.toString()}
+                        addItem={addItem}
+                        item={item}
+                        state={order}
+                      />
+                    );
+                  })
+                : dinner.map((item) => {
+                    return (
+                      <ProductCard
+                        key={item.id.toString()}
+                        addItem={addItem}
+                        item={item}
+                        state={order}
+                      />
+                    );
+                  })}
+            </>
+          </div>
+          <img
+            className="logo-person"
+            src={require("../../../images/person-pin.png")}
+            alt="Person icon"
           />
-          <Button
-            filter={changeMenu}
-            value="dinner"
-            name="Lunch & Dinner"
-            secondclass="center"
-          />
-          <div className="lineOne" />
-        </section>
-        <div className="product-card-container">
-          <>
-            {optionFood === "breakfast"
-              ? breakfast.map((item) => {
-                  return (
-                    <ProductCard
-                      key={item.id.toString()}
-                      addItem={addItem}
-                      item={item}
-                      state={order}
-                    />
-                  );
-                })
-              : dinner.map((item) => {
-                  return (
-                    <ProductCard
-                      key={item.id.toString()}
-                      addItem={addItem}
-                      item={item}
-                      state={order}
-                    />
-                  );
-                })}
-          </>
+          <p className="waiter-name">{user}</p>
         </div>
-        <img
-          className="logo-person"
-          src={require("../../../images/person-pin.png")}
-          alt="Person icon"
-        />
-        <p className="waiter-name">{user}</p>
-      </main>
-      <section className="order-summary-container">
-        <h3 className="order-summary-text">Order summary</h3>
-        <form onSubmit={order.length === 0 ? showEmptyOrderMessage : showModal}>
-          <div className="input-customer-name">
-            <label>Customer's Name </label>
-            <input
-              className="customer-name-input"
-              type="text"
-              onChange={handleChange}
-              value={clientName}
-              required
-            ></input>
-          </div>
-          <div className="order-container">
-            <div className="order-description">
-              <table>
-                <thead>
-                  <tr>
-                    <th> QTY </th>
-                    <th className="style-name-column"> Name </th>
-                    <th> Price </th>
-                    <th className="style-delete-column"> Delete </th>
-                  </tr>
-                </thead>
-                <>
-                  {order != []
-                    ? order.map((item) => {
-                        return (
-                          <OrderDetails
-                            key={item.product.id.toString()}
-                            qty={item.qty}
-                            item={item}
-                            name={item.product.name}
-                            price={item.product.price}
-                            removeItem={removeItem}
-                            deleteItem={deleteItem}
-                          />
-                        );
-                      })
-                    : console.log("no hay nada")}
-                </>
-              </table>
+
+        <section className="order-summary-container">
+          <h3 className="order-summary-text">Order summary</h3>
+          <form
+            onSubmit={order.length === 0 ? showEmptyOrderMessage : showModal}
+          >
+            <div className="input-customer-name">
+              <label>Customer's Name </label>
+              <input
+                className="customer-name-input"
+                type="text"
+                onChange={handleChange}
+                value={clientName}
+                required
+              ></input>
             </div>
-            <p className="price"> Total price </p>
-            <p className="number-price">$ {total()}.00</p>
-            <SendButton name="Send to kitchen" secondclass="orders" />
-            {emptyOrderMessage ? <div className="empty-order-message">Fill the order with products</div> : showModal}
-            <ReactModal
-              isOpen={modal}
-              className="Modal"
-              overlayClassName="Overlay"
-              ariaHideApp={false}
-            >
-              <>
-                {message === false ? (
-                  <div className="confirmation-container">
-                    <h2 className="modal-text">
-                      Do you want to send this order to the kitchen?
-                    </h2>
-                    <button
-                      className="modal-button-left"
-                      onClick={handleOrderInformation}
-                    >
-                      YES
-                    </button>
-                    <button className="modal-button-right" onClick={closemodal}>
-                      NO
-                    </button>
-                  </div>
-                ) : (
-                  <div className="successful-message">
-                    <h2 className="modal-text">
-                      {modalMessage}
-                    </h2>
-                  </div>
-                )}
-              </>
-            </ReactModal>
-          </div>
-        </form>
-      </section>
+            <div className="order-container">
+              <div className="order-description">
+                <table>
+                  <thead>
+                    <tr>
+                      <th> QTY </th>
+                      <th className="style-name-column"> Name </th>
+                      <th> Price </th>
+                      <th className="style-delete-column"> Delete </th>
+                    </tr>
+                  </thead>
+                  <>
+                    {order != []
+                      ? order.map((item) => {
+                          return (
+                            <OrderDetails
+                              key={item.product.id.toString()}
+                              qty={item.qty}
+                              item={item}
+                              name={item.product.name}
+                              price={item.product.price}
+                              removeItem={removeItem}
+                              deleteItem={deleteItem}
+                            />
+                          );
+                        })
+                      : console.log("no hay nada")}
+                  </>
+                </table>
+              </div>
+              <p className="price"> Total price </p>
+              <p className="number-price">$ {total()}.00</p>
+              <SendButton name="Send to kitchen" secondclass="orders" />
+              {emptyOrderMessage ? (
+                <div className="empty-order-message">
+                  Fill the order with products
+                </div>
+              ) : (
+                showModal
+              )}
+              <ReactModal
+                isOpen={modal}
+                className="Modal"
+                overlayClassName="Overlay"
+                ariaHideApp={false}
+              >
+                <>
+                  {message === false ? (
+                    <div className="confirmation-container">
+                      <h2 className="modal-text">
+                        Do you want to send this order to the kitchen?
+                      </h2>
+                      <button
+                        className="modal-button-left"
+                        onClick={handleOrderInformation}
+                      >
+                        YES
+                      </button>
+                      <button
+                        className="modal-button-right"
+                        onClick={closemodal}
+                      >
+                        NO
+                      </button>
+                    </div>
+                  ) : (
+                    <div className="successful-message">
+                      <h2 className="modal-text">{modalMessage}</h2>
+                    </div>
+                  )}
+                </>
+              </ReactModal>
+            </div>
+          </form>
+        </section>
+      </main>
     </div>
   );
 }
--- burguerqueen/src/components/generalComponents/NavBar.jsx
+++ burguerqueen/src/components/generalComponents/NavBar.jsx
@@ -7,15 +7,17 @@ function NavBar({ changeUser }) {
   }
   return (
     <nav className="superiorNav">
-      <img
-        className="logo-logout"
-        src={require("../../../src/images/logout.png")}
-        alt="Logout"
-        onClick={logOut}
-      />
-      <button className="logout-button" onClick={logOut}>
-        Logout
-      </button>
+      <div>
+        <img
+          className="logo-logout"
+          src={require("../../../src/images/logout.png")}
+          alt="Logout"
+          onClick={logOut}
+        />
+        <button className="logout-button" onClick={logOut}>
+          Logout
+        </button>
+      </div>
       <img
         className="logo-waiter-layout"
         src={require("../../../src/images/queen.png")}
